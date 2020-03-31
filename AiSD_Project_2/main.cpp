74094d6e4f2c4d358bfbc8a31d4dee53da200f31
Kacper Kedzierski <kacper.kedzierski1986@gmail.com>
2020-03-31 14:11:46 +0200

    Initial Commit

---
+++ AiSD_Project_2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AiSD_Project_2
+//
+//  Created by Kacper Kędzierski on 31/03/2020.
+//  Copyright © 2020 Kacper Kędzierski. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
